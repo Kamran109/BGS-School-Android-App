f6c793c50ca7cee432f251a425106a71377023ba
Kami109 <softwaredocs36@gmail.com>
2026-04-09 15:44:04 +0500

    Git File

--- app/src/main/java/com/david/quizuppro/test/gitcommands/GitCommands.kt
+++ app/src/main/java/com/david/quizuppro/test/gitcommands/GitCommands.kt
@@ -1,14 +1,26 @@
-//package com.david.quizuppro.test.gitcommands
-
-
+//                                          (1)
 /*<------------------------------------Status check krna------------------------------------>*/
 // konsi file me kya changes huwe hain
 
 // git status
 
+//                                         (2)
+/*<------------------------------------Add Changes------------------------------------>*/
+// sab files ko add krna jis me changes huwe hain
 
-/*<------------------------------------Status check krna------------------------------------>*/
-// konsi file me kya changes huwe hain
+// git add .
 
-// git status
+//                                         (3)
+/*<------------------------------------Commit Message------------------------------------>*/
+// jo chages kiye hain us k bare me comment krna
+
+// git commit -m "Message"
+
+//                                         (4)
+/*<------------------------------------Upload Changes on github------------------------------------>*/
+// changes github pr upload krna
+
+// git push origin main
+
+
 
